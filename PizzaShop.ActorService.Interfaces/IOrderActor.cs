0ee678ac478c147a915faa9081fe43bdf06a5c0c
jstuparitz <jstuparitz@hotmail.com>
2015-07-20 14:54:53 -0500

    Added IOrderActor interface

---
+++ PizzaShop.ActorService.Interfaces/IOrderActor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Actors;
+using PizzaShop.Commands.Order;
+
+namespace PizzaShop.ActorService.Interfaces
+{
+    /// <summary>
+    ///     Actors interact with rest of the system, including other actors, by passing asynchronous messages using
+    ///     a request-response pattern. These interactions are defined in an interface as asynchronous methods.
+    ///     Arguments and the result type of the Task it returns must be serializable.
+    /// </summary>
+    public interface IOrderActor : IActor
+    {
+        Task<Guid> CreateOrder(CreateOrderCommand command);
+        Task CancelOrder(CancelOrderCommand command);
+        Task<string> CheckOrderStatus(CheckOrderStatusCommand command);
+        Task CompleteOrder(CompleteOrderCommand command);
+    }
+}
