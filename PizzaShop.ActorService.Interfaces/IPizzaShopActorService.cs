2f4f79fa4a7c6669ca897dc31ffea46c7c7416c0
jstuparitz <jstuparitz@hotmail.com>
2015-07-20 14:43:42 -0500

    Initial Commit

---
+++ PizzaShop.ActorService.Interfaces/IPizzaShopActorService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Actors;
+
+namespace PizzaShop.ActorService.Interfaces
+{
+    public interface IPizzaShopActorService : IActor
+    {
+        Task<int> GetCountAsync();
+
+        Task SetCountAsync(int count);
+    }
+}
---
+++ PizzaShop.ActorService/PizzaShopActorServiceState.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using PizzaShop.ActorService.Interfaces;
+using Microsoft.ServiceFabric;
+using Microsoft.ServiceFabric.Actors;
+
+namespace PizzaShop.ActorService
+{
+    [DataContract]
+    public class PizzaShopActorServiceState
+    {
+        [DataMember]
+        public int Count;
+
+        public override string ToString()
+        {
+            return string.Format(CultureInfo.InvariantCulture, "PizzaShopActorServiceState[Count = {0}]", Count);
+        }
+    }
+}
